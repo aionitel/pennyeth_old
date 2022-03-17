fa3075b69a846944c46f6029c3a2ab05038ebc59
Alex Ionitel <oranggsoda@gmail.com>
2022-03-16 23:12:29 -0400

    connecting to wallet basics working

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,20 @@
 import type { AppProps } from 'next/app'
+import { DAppProvider, Config } from '@usedapp/core'
+import { Mainnet } from '@usedapp/core'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const config = {
+  readOnlyChainId: Mainnet.chainId,
+  readOnlyUrls: {
+    [Mainnet.chainId]: 'https://mainnet.infura.io/v3/62687d1a985d4508b2b7a24827551934',
+  },
+}
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    <DAppProvider config={config}>
+      <Component {...pageProps} /> 
+    </DAppProvider>
+  )
 }
 
 export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
 import type { NextPage } from 'next'
+import { useEthers } from '@usedapp/core'
 
 const Home: NextPage = () => {
+  const { activateBrowserWallet, account } = useEthers()
+
   return (
-    <div>
-      Hello
+    <div className='flex justify-center'>
+      {!account && <button onClick={() => activateBrowserWallet()}>Connect</button>}
+      {account && <h1>Account: {account}</h1>}
     </div>
   )
 }
