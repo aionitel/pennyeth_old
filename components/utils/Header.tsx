21a014543f95483ef65b30a0039a7ebc521e4ce5
Alex Ionitel <oranggsoda@gmail.com>
2022-04-20 22:01:56 -0400

    better tooltip library

--- components/utils/Header.tsx
+++ components/utils/Header.tsx
@@ -16,11 +16,9 @@ const Header = () => {
       <>
         <div className='justify-between bg-black items-center text-white h-20 hidden sm:flex border-b border-lightgray'>
           <h1 className='text-4xl ml-10'>{currPage}</h1>
-          <button data-tooltip-target="tooltip-dark" type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Dark tooltip</button>
-          <div id="tooltip-dark" role="tooltip" className="inline-block absolute invisible z-10 py-2 px-3 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
-            Tooltip content
-          <div className="tooltip-arrow" data-popper-arrow></div>
-          </div>
+          <button>
+            <div className='border-white mr-8' data-event='click' data-tip='hello world`'>Press me</div>
+          </button>
         </div>
         <Modal isOpen={modalVisible} />
       </>
