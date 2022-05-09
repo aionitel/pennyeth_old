f375fb2e71cd1f7c33c4b3637f78998eb8451b1c
Alex Ionitel <oranggsoda@gmail.com>
2022-05-08 21:19:27 -0400

    closing and opening login modal working

--- components/connectCrypto/Metmask.tsx
+++ components/connectCrypto/Metmask.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Metmask: React.FC = () => {
   return (
     <div>
-      
+      <button>Connect with Metamask</button> 
     </div>
   )
 }
--- components/user/LoggedOutHeader.tsx
+++ components/user/LoggedOutHeader.tsx
@@ -1,18 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useMoralis } from 'react-moralis'
+import Modal from 'react-modal'
+import Metmask from '../connectCrypto/Metmask'
+
 // Connect button that allows for user to connect eth or sol wallet with modal
 
 const LoggedOutHeader = () => {
-  const { Moralis } = useMoralis()
-
-  const handleLogin = () => {
-    const user = Moralis.authenticate()
-  }
+  const [modalOpen, setModalOpen] = useState(false)
 
   return (
-    <button className='bg-blue text-white mr-8 py-2 px-2 hover:scale-105 transition-all rounded-sm'>
+    <>
+    <button 
+      className='bg-blue text-white mr-8 py-2 px-2 hover:scale-105 transition-all rounded-sm'
+      onClick={() => setModalOpen(true)}
+    >
       <h1>Connect Wallet</h1>
     </button>
+    <Modal
+        isOpen={modalOpen}
+        onRequestClose={() => setModalOpen(false)}
+        shouldCloseOnEsc={true}
+        shouldCloseOnOverlayClick={true}
+        style={{
+          overlay: {
+            position: 'fixed',
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            backgroundColor: 'rgba(255, 255, 255, 0.75)'
+          },
+          content: {
+            position: 'absolute',
+            top: '200px',
+            left: '100x',
+            right: '100px',
+            bottom: '200px',
+            border: '1px solid #ccc',
+            background: '#fff',
+            overflow: 'auto',
+            WebkitOverflowScrolling: 'touch',
+            borderRadius: '4px',
+            outline: 'none',
+            padding: '20px'
+          }
+        }}
+      >
+        <div>
+          <Metmask />
+        </div>
+      </Modal>
+    </>
   )
 }
 
