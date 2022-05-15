23c1bbb7322aed97f45578b78e43e26437e85273
Alex Ionitel <oranggsoda@gmail.com>
2022-05-14 23:52:44 -0400

    logging in and logging out with metamask working

--- components/header/LoggedInHeader.tsx
+++ components/header/LoggedInHeader.tsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import { currUserAtom } from '../../state/atoms'
-import { useRecoilValue } from 'recoil'
+import { useRecoilState } from 'recoil'
 
 // when user has connected wallet, show wallet address in header
 
 const LoggedInHeader = () => {
-  const user = useRecoilValue(currUserAtom)
+  const [user, setUser] = useRecoilState(currUserAtom)
   
   return (
     <div className='flex-col text-center text-white border border-white rounded-3xl bg-green-400'>
       <h1>Connected</h1>
       <h2>{user}</h2>
+      <button onClick={() => setUser("")}>
+        <h1>Logout</h1>
+      </button>
     </div>
   )
 }
