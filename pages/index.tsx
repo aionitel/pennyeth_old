502114754a9b36ca91f03c99a9cb8c749a50ae92
Alex Ionitel <oranggsoda@gmail.com>
2022-05-14 21:42:27 -0400

    home functionality working

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,14 @@
 import { NextPage } from 'next'
 import React from 'react'
+import { currPageAtom } from '../state/atoms'
+import { useRecoilState } from 'recoil'
+import { useEffect } from 'react'
 
 const Home: NextPage = () => {
+  const [currPage, setCurrPage] = useRecoilState(currPageAtom)
+
+  useEffect(() => setCurrPage('Home'))
+
   return (
     <div>Home</div>
   )
