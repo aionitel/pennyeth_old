0abbe676a91cf875f87c3b50e59393eef3c8333f
Alex Ionitel <oranggsoda@gmail.com>
2022-05-15 00:11:56 -0400

    using recoil-persist to keep user state (ethAddress) when reloading page

--- components/nav/NavBar.tsx
+++ components/nav/NavBar.tsx
@@ -3,17 +3,26 @@ import NavItem from './NavItem'
 import { BiCoin } from 'react-icons/bi'
 import NavBarItemsData from './NavItemData'
 import Link from 'next/link'
+import ConnectWallet from '../connectWallet/ConnectWallet'
+import { useRecoilValue } from 'recoil'
+import { currUserAtom } from '../../state/atoms'
+import LoggedInHeader from '../header/LoggedInHeader'
 
 // desktop side navbar
 
 const NavBar: React.FC = () => {
+  const user = useRecoilValue(currUserAtom)
+
   return (
     <nav className="bg-black border-r border-r-lightgray h-[1] hidden md:block text-white">
-      <div className='flex my-8 ml-7 text-3xl'>
-        <BiCoin className='pt-1' />
-        <Link href='/secret' passHref>
-          <h1 className='ml-1 text-md hover:cursor-pointer'>PennyETH</h1>
-        </Link>
+      <div className='flex-col my-8 ml-7 text-3xl'>
+        <div className='flex my-5'>
+          <BiCoin className='pt-1' />
+          <Link href='/secret' passHref>
+            <h1 className='ml-1 text-md hover:cursor-pointer'>PennyETH</h1>
+          </Link>
+        </div>
+        {user ? <LoggedInHeader /> : <ConnectWallet />}
       </div>
       <ul>
         {NavBarItemsData.map(item => (
--- state/atoms.ts
+++ state/atoms.ts
@@ -1,16 +1,22 @@
 import { atom } from "recoil";
+import { recoilPersist } from "recoil-persist";
+
+const { persistAtom } = recoilPersist();
 
 export const currPageAtom = atom({ // keeping track of current page 
   key: 'currPage',
-  default: 'Assets' as string
+  default: 'Assets' as string,
+  effects_UNSTABLE: [persistAtom]
 })
 
 export const currUserAtom = atom({ // global user state
   key: 'currUser',
-  default: '' as string 
+  default: '' as string ,
+  effects_UNSTABLE: [persistAtom]
 })
 
 export const navOpenAtom = atom({ // to close and open modal on certain events
   key: 'modalOpen',
-  default: false as boolean
+  default: false as boolean,
+  effects_UNSTABLE: [persistAtom]
 })
