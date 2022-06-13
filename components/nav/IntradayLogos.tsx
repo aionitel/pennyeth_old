ee00604a9bf3289c859f4f5fb870603cc679371a
Alex Ionitel <oranggsoda@gmail.com>
2022-06-12 23:34:25 -0400

    better IntradayLogos spacing for logo and percent change

--- components/nav/IntradayLogos.tsx
+++ components/nav/IntradayLogos.tsx
@@ -1,20 +1,34 @@
 import Image from 'next/image'
 import React from 'react'
+import { motion } from 'framer-motion';
 
 const IntradayLogos: React.FC = () => {
   const logoSize = 30;
 
   return (
-    <div className='flex text-base'>
-      <div className='flex hover:cursor-pointer'>
-        <Image src='https://i.imgur.com/wbZ6UVD.png' height={logoSize} width={logoSize} alt='btc_logo'/>
-        <h1 className='text-red'>-10.6%</h1>
+    <motion.div 
+      className='flex text-base justify-center my-2 ml-2'
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.5 }}
+    >
+      <div className='flex hover:cursor-pointer mr-8'>
+        <Image 
+          src='https://i.imgur.com/wbZ6UVD.png' 
+          height={logoSize}
+           width={logoSize} 
+           alt='btc_logo'/>
+        <h1 className='text-red pt-1 mx-2'>-10.6%</h1>
       </div>
-      <div className='flex hover:cursor-pointer'>
-        <Image src='https://i.imgur.com/izIV4k9.png' height={logoSize} width={logoSize} alt='eth_logo' />
-        <h1 className='text-red'>-5.8%</h1>
+      <div className='flex hover:cursor-pointer mr-10'>
+        <Image 
+          src='https://i.imgur.com/izIV4k9.png' 
+          height={logoSize} 
+          width={logoSize} 
+          alt='eth_logo' />
+        <h1 className='text-red pt-1 mx-3'>-5.8%</h1>
       </div>
-    </div>
+    </motion.div>
   )
 }
 
