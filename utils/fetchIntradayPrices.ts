8aa92c6c7774210b4dbb41f2406ef06b0f31e36b
Alex Ionitel <oranggsoda@gmail.com>
2022-06-13 20:26:22 -0400

    finished fetchPrices (now fetchIntradayPrices)

--- utils/fetchIntradayPrices.ts
+++ utils/fetchIntradayPrices.ts
@@ -1,11 +1,16 @@
 import axios from 'axios';
 
-const fetchPrices = async () => {
+const fetchIntradayPrices = async () => {
   const btc_url = `https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=BTC&to_currency=USD&apikey=${process.env.API_KEY}`;
   const eth_url = `https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=ETH&to_currency=USD&apikey=${process.env.API_KEY}`;
 
   const btc_response = await axios.get(btc_url);
   const eth_response = await axios.get(eth_url);
+
+  const btc_price = btc_response.data['Realtime Currency Exchange Rate']['5. Exchange Rate'];
+  const eth_price = eth_response.data['Realtime Currency Exchange Rate']['5. Exchange Rate'];
+
+  return { btc_price, eth_price };
 }
 
-export default fetchPrices;
+export default fetchIntradayPrices;
