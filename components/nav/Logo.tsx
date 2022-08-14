cfc96d12e63b027c50d300112669955bb42539bb
Alex Ionitel <oranggsoda@gmail.com>
2022-08-14 01:15:37 -0400

    production build working on mobile, fetching all data on initial page load in Logo

--- components/nav/Logo.tsx
+++ components/nav/Logo.tsx
@@ -3,10 +3,10 @@ import { useRecoilState } from 'recoil';
 import { assetMetricsAtom, newsAtom, WeeklyBtcAtom, WeeklyEthAtom } from '../../state/atoms';
 import fetchWeeklyBtc from '../../data/prices/btc/fetchWeeklyBtc';
 import fetchWeeklyEth from '../../data/prices/eth/fetchWeeklyEth';
-import fetchAssetMetrics from '../../data/prices/fetchAssetMetrics';
 import fetchNews from '../../data/news/fetchNews';
 import images from '../../data/images/images';
 import { BiCoin } from 'react-icons/bi'
+import fetchAssetData from '../../data/prices/misc/fetchAssetData';
 
 const Logo = () => {
   const [currAssetMetrics, setCurrAssetMetrics] = useRecoilState(assetMetricsAtom);
@@ -25,15 +25,15 @@ const Logo = () => {
       const weeklyEth = await fetchWeeklyEth();
 
       // various market data for specified asset
-      const btcData = await fetchAssetMetrics("btc");
+      const btcData = await fetchAssetData("btc");
       btcData.image = 'https://i.imgur.com/wbZ6UVD.png'
-      const ethData = await fetchAssetMetrics("eth");
+      const ethData = await fetchAssetData("eth");
       ethData.image = 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fclipground.com%2Fimages%2Fethereum-png-12.png&f=1&nofb=1'
-      const xmrData = await fetchAssetMetrics("xmr");
+      const xmrData = await fetchAssetData("xmr");
       xmrData.image = 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwiki.trezor.io%2Fimages%2FMonero.png&f=1&nofb=1'
-      const solData = await fetchAssetMetrics("sol");
+      const solData = await fetchAssetData("sol");
       solData.image = 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fyourcryptolibrary.com%2Fwp-content%2Fuploads%2F2021%2F05%2Fsolana-sol-logo.png&f=1&nofb=1'
-      const adaData = await fetchAssetMetrics("ada");
+      const adaData = await fetchAssetData("ada");
       adaData.image = 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ficons-for-free.com%2Ficonfiles%2Fpng%2F512%2Fcardano%2Bicon-1320162855683510157.png&f=1&nofb=1'
 
       // set global price for each asset to be used later in app
