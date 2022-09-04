3e1422c82bd10c72f0c77244bb21564b5076c1bc
Alex Ionitel <oranggsoda@gmail.com>
2022-09-04 11:05:33 -0400

    finished fetchAddress()

---
+++ data/explorer/fetchAddr.ts
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+interface Address {
+  address: string;
+  totalReceived: number;
+  totalSent: number;
+  balance: number,
+  n_txs: number;
+  txs: string[]; // list of tx hashes belonging to address
+}
+
+const fetchAddr = async (ticker: string, addr: string) => {
+  const url = `https://api.blockcypher.com/v1/${ticker}/main/txs/${addr}`;
+
+  const { data: res } = await axios.get(url);
+
+  const address: Address = {
+    address: res.address,
+    totalReceived: res.total_received,
+    totalSent: res.total_sent,
+    balance: res.balance,
+    n_txs: res.n_tx,
+    txs: res.txrefs
+  }
+
+  return address;
+}
+
+export default fetchAddr;
